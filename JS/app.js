8d4a404f6d4f7466331f0f11ea70c8549e4b1fe6
ElsaLecureux <elsa.lecureux@gmail.com>
2022-09-17 15:33:11 +0200

    retouche css

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+//Je souhaite créer une page d'accueil présentant deux animations loading:
+//une pour faire un signe avec des ronds qui tournent 
+// l'autre pour faire une chargement qui augmente en quelques secondes, quand il est à 100% une bouton apparait
+
+//au clic du bouton, m'a page CV se crée en JS
+
+//créer la page:
+// dans mon HEADER:
+// je souhaite avoir ma photo avec mon nom, mon activité, les liens vers mon comptes (github, linkedin,mail)
+
+// dans le MAIN:
+// ma présentation
+// présentation de ma formation?
+// mes projets
+
+//dans le FOOTER:
+// juste paragraphe nom prenom, année copyright
