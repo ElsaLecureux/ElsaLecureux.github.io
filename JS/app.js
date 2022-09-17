c6c81a02f6eb1ced16c23c63d098290a2808d945
ElsaLecureux <elsa.lecureux@gmail.com>
2022-09-17 16:19:51 +0200

    commentaires pour formuler mon travail JS

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,16 @@
 //créer la page:
 // dans mon HEADER:
 // je souhaite avoir ma photo avec mon nom, mon activité, les liens vers mon comptes (github, linkedin,mail)
+// Je crée une div avec la class 'container_name_and_work' pour y mettre mes titres
+//Je crée un h1 avec le textContent 'Elsa Lécureux'
+//Je crée un h2 vace une class 'title_work' et le textContent 'Développeuse en cours formation'
+
+//Je crée une div avec la class 'container_profil' pour y mettre ma photo mes liens
+//je crée une div avec la class 'container_photo-profil' pour accueilir ma photo et la styliser ensuite
+//J'ajoute un element img au DOM dans ma div avec la class 'photo-profil'
+
+//Je crée une div avec la class 'cointainer_links pour contenir mes liens
+//J'ajoute mes 3 liens avec leurs images
 
 // dans le MAIN:
 // ma présentation
