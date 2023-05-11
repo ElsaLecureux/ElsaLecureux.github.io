f8638a35a2d06eabf44e248d6c9ce0b88e133a33
ElsaLecureux <elsa.lecureux@gmail.com>
2023-05-11 11:28:53 +0200

    cleaning up and transtion for leftbutton

--- JS/app.js
+++ JS/app.js
@@ -6,9 +6,8 @@ const app = {
         app.addListenerToCarouselItems();
         app.addListenerToCloseModal();
     },
-    CarouselElm: document.querySelector('.carousel'),
     CarouselWrapElm: document.querySelector('.carousel_wrap'),
-    Slide: document.querySelectorAll('.carousel_item'),
+    CarouselItemsElm: document.querySelectorAll('.carousel_item'),
     LeftButtonElm: document.querySelector('.LeftButton'),
     RightButtonElm: document.querySelector('.RightButton'),
     ModalElm: document.querySelector('.modal'),
@@ -28,23 +27,19 @@ const app = {
         
         if (app.index > 0) {
             app.index = app.index - 1;
-            /*const slideWidth = app.Slide.clientWidth;
-            app.CarouselWrapElm.scrollLeft -= slideWidth;*/
-            document.body.style.background= app.backgroundColors[app.index];
-            /*app.Slide[app.index].style.opacity = '0';*/
+            document.body.style.background= app.backgroundColors[app.index];      
+            const transition = document.querySelector('.body');
+            app.CarouselItemsElm[app.index+1].style.opacity = '0';
             setTimeout(() => {
                 console.log("Retardée d'une seconde.");
-              }, 1000)        
-            app.Slide[app.index+1].style.opacity = '0';
-            app.Slide[app.index+1].classList.toggle('selected');
-            app.Slide[app.index].style.opacity = '1';
-            app.Slide[app.index].classList.toggle('selected');               
-            const transition = document.querySelector('.body');
-            
-            
- 
-           
-               
+                app.CarouselItemsElm[app.index+1].classList.toggle('selected');
+                app.CarouselItemsElm[app.index].style.opacity = '0';
+                app.CarouselItemsElm[app.index].classList.toggle('selected');                
+            }, 805)
+            setTimeout(() => {
+                console.log("Retardée plus tard.");
+                app.CarouselItemsElm[app.index].style.opacity = '1';
+              }, 820)
         } 
         if (app.index === 0) {
             app.LeftButtonElm.classList.add('visible');  
@@ -58,23 +53,15 @@ const app = {
         app.index = app.index + 1;
         document.body.style.background= app.backgroundColors[app.index];
         const transition = document.querySelector('.body');
-        app.Slide[app.index-1].style.opacity = '0';
+        app.CarouselItemsElm[app.index-1].style.opacity = '0';
         setTimeout(() => {
-            console.log("Retardée d'une seconde.");
-            app.Slide[app.index-1].classList.toggle('selected');
-            app.Slide[app.index].classList.toggle('selected');               
-            app.Slide[app.index].style.opacity = '0';
+            app.CarouselItemsElm[app.index-1].classList.toggle('selected');
+            app.CarouselItemsElm[app.index].style.opacity = '0';
+            app.CarouselItemsElm[app.index].classList.toggle('selected');               
         }, 805)
         setTimeout(() => {
-            console.log("Retardée plus tard.");
-            app.Slide[app.index].style.opacity = '1';
-                           
+            app.CarouselItemsElm[app.index].style.opacity = '1';
           }, 820)
-           /* app.Slide[app.index].style.opacity = '0';*/
-        
-       
-        /*const slideWidth = app.Slide.clientWidth;
-        app.CarouselWrapElm.scrollLeft += slideWidth;*/
         }
         if (app.index > 0) {
             app.LeftButtonElm.classList.remove('visible');  
@@ -85,8 +72,7 @@ const app = {
     },    
 
     addListenerToCarouselItems () {
-        const CarouselItemsElm = app.CarouselWrapElm.querySelectorAll('.carousel_item');
-       for (const itemElm of CarouselItemsElm) {
+       for (const itemElm of app.CarouselItemsElm) {
         itemElm.addEventListener('click', app.onClickItem);
       }    
     },
@@ -102,9 +88,6 @@ const app = {
             }  
         }
         app.ModalElm.style.display = 'block'; 
-        /*app.CarouselWrapElm.style.display = 'none';
-        app.LeftButtonElm.style.display = 'none';
-        app.RightButtonElm.style.display = 'none';*/ 
     },         
     
     addListenerToCloseModal () {
